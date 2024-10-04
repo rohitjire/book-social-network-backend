a1356d7d00c7121ee071b07e627f321483f226ec
Rohit Jire <rohitjire55@gmail.com>
2024-10-04 13:05:12 +0530

    Intial commit

---
+++ src/main/java/com/personal/booksocialnetwork/BookSocialNetworkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.personal.booksocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialNetworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialNetworkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/booksocialnetwork/BookSocialNetworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.booksocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialNetworkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
